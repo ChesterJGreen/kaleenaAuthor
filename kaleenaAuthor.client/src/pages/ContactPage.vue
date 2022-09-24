bf72ff3e713259e230246575d4aa004b1ce217ff
chesterjgreen <seejarom@gmail.com>
2022-09-23 23:59:00 -0600

    creating email template 1

--- kaleenaAuthor.client/src/pages/ContactPage.vue
+++ kaleenaAuthor.client/src/pages/ContactPage.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="about container-fluid my-5">
       <div class="row">
-        <div class="clo-12 mt-5">
+        <div class="col-12 mt-5">
           <h1>This is the contact page</h1>
+        </div>
+        <div class="col-12 mt-5">
+          <div class="row">
+            <div class="col-12">
+              
+            </div>
+          </div>
         </div>
       </div>
     </div>
