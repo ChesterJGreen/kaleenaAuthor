693c59a1abf9a9be4c9312bab5bf285ee9c1fdae
chesterjgreen <seejarom@gmail.com>
2022-10-19 22:55:23 -0600

    adding blog guts

---
+++ kaleenaAuthor.client/src/components/BlogCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="row">
+
+    </div>
+</template>
+
+<script>
+import { computed, reactive } from '@vue/runtime-core'
+import { AppState } from '../AppState'
+
+export default {
+    name: 'BlogCard',
+    props: {
+        blog: {
+            type: Object,
+            required: true
+        }
+    },
+    setup(props) {
+        const state = reactive({
+            blog: blogs.blog
+        })
+        return {
+            state,
+            blogs: computed(() => AppState.blogs)
+        }
+    }
+}
+</script>
+<style>
+
+</style>
