b7e87586ffca6bd518f83b0891266cf0925e12a0
chesterjgreen <seejarom@gmail.com>
2022-11-10 23:58:36 -0700

    added books

--- kaleenaAuthor/Models/Blog.cs
+++ kaleenaAuthor/Models/Blog.cs
@@ -2,7 +2,8 @@ using System;
 
 namespace kaleenaAuthor.Models
 {
-    public class Blog{
+    public class Blog
+    {
         public int Id {get;set;}
         public string Name {get;set;}
         public string Body {get;set;}
---
+++ kaleenaAuthor/Models/Book.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kaleenaAuthor.Models
+{
+    public class Book
+    {
+        public int Id {get; set;}
+        public string Name {get;set;}
+        public int Progress {get;set;}
+    }
+}
