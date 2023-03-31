77da5678109b0bffdf87e0cfc93f931a8325fa77
@ChesterJGreen <chester.green208@gmail.com>
2023-03-30 23:40:04 -0600

    menubar formatting!

--- kaleenaAuthor.client/src/components/Navbar.vue
+++ kaleenaAuthor.client/src/components/Navbar.vue
@@ -3,76 +3,84 @@
     <nav class="row navbar navbar-expand-lg">
       <div class="col-12">
         <div class="row">
-          <div class="col-4">
-      <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-        <div class="d-flex flex-column align-items-center">
-           <img src="src\assets\img\LogoTransparentEnlargedCropped.png" class="rounded-pill w-75" alt="">
+          <div class="col-10">
+          </div>
+          <div class="col-2">
+            
+          </div>
         </div>
-      </router-link>
-    </div>
-    </div>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon" />
-    </button>
-    <div class="collapse navbar-collapse pt-3" id="navbarText">
-      <ul class="navbar-nav me-auto text-dark volk">
-        <li>
-          <router-link :to="{ name: 'About' }" class="router btn text-dark selectable text-uppercase">
-            About Me
-          </router-link>
-        </li>
-        <li>
-          <router-link :to="{ name: 'Books' }" class="router btn text-dark selectable text-uppercase">
-            Books
-          </router-link>
-        </li>
-        <li>
-          <router-link :to="{ name: 'Journeys' }" class="router btn text-dark selectable text-uppercase">
-            Journeys
+        <div class="row">
+          <div class="col-3">
+            <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+              <div class="d-flex flex-column align-items-center">
+                <img src="src\assets\img\LogoTransparentEnlargedCropped.png" class="rounded-pill w-75" alt="">
+              </div>
             </router-link>
-          </li>
-          <li>
-            <router-link :to="{ name: 'Blog' }" class="router btn text-dark selectable text-uppercase">
-              Blog
-            </router-link>
-          </li>
-          <li>
-            <router-link :to="{ name: 'Contact' }" class="router btn text-dark selectable text-uppercase">
-              Contact
-            </router-link>
-          </li>
-          
-          <li>
-            <router-link :to="{ name: 'Admin' }" class="router btn text-dark selectable text-uppercase">
-              Admin Controls
-            </router-link>
-          </li>
-        </ul>
-        <ul class="navbar-nav justify-content-right justify-content-end">
-          <ul>
-            <a href="https://www.facebook.com/kaleena.shreeve"><img width="75"
-              src="src\assets\img\facebookLogo.png"></a>
-            </ul>
-            <ul>
-              <a href="https://www.instagram.com/kaleenashreeve1/"><img width="75"
-                src="src\assets\img\InstagramLogo.png"></a>
+          </div>
+          <div class="col-8">
+            <div class="collapse navbar-collapse pt-3" id="navbarText">
+              <ul class="navbar-nav justify-content-right justify-content-end">
+                <ul class="navbar-nav me-auto text-dark volk">
+                  <li>
+                    <router-link :to="{ name: 'About' }" class="router btn text-dark selectable text-uppercase">
+                      About Me
+                    </router-link>
+                  </li>
+                  <li>
+                    <router-link :to="{ name: 'Books' }" class="router btn text-dark selectable text-uppercase">
+                      Books
+                    </router-link>
+                  </li>
+                  <li>
+                    <router-link :to="{ name: 'Journeys' }" class="router btn text-dark selectable text-uppercase">
+                      Journeys
+                    </router-link>
+                  </li>
+                  <li>
+                    <router-link :to="{ name: 'Blog' }" class="router btn text-dark selectable text-uppercase">
+                      Blog
+                    </router-link>
+                  </li>
+                  <li>
+                    <router-link :to="{ name: 'Contact' }" class="router btn text-dark selectable text-uppercase">
+                      Contact
+                    </router-link>
+                  </li>
+                  <li>
+                    <router-link :to="{ name: 'Admin' }" class="router btn text-dark selectable text-uppercase">
+                      Admin Controls
+                    </router-link>
+                  </li>
+                  <li>
+                    <a href="https://www.facebook.com/kaleena.shreeve"><img width="75"
+                        src="src\assets\img\facebookLogo.png"></a>
+                  </li>
+                  <li>
+                    <a href="https://www.instagram.com/kaleenashreeve1/"><img width="75"
+                        src="src\assets\img\InstagramLogo.png"></a>
+                  </li>
+                  <li>
+                    <a href="https://www.pinterest.com/kaleenashreeve/"><img width="75"
+                        src="src\assets\img\PinterestLogo.png"></a>
+                  </li>
+                  <li>
+                    <span class="p-1 ">
+                      <button class="btn text-white rounded-pill bg-primary volk p-1.5" data-toggle="modal"
+                        data-target="#newsletterModal">Newsletter</button>
+                    </span>
+                  </li>
+                </ul>
               </ul>
-          <ul>
-            <a href="https://www.pinterest.com/kaleenashreeve/"><img width="75"
-              src="src\assets\img\PinterestLogo.png"></a>
-            </ul>
-          <ul>
-            <span class="p-1 ">
-              <button class="btn text-white rounded-pill bg-primary volk p-1.5" data-toggle="modal" data-target="#newsletterModal">Newsletter</button>
-            </span>
-          </ul>
-        </ul>
+            </div>
+          </div>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon" />
+          </button>
+        </div>
       </div>
-    </div>
     </nav>
   </div>
-  
 </template>
 
 <script>
@@ -97,13 +105,16 @@ a:hover {
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
 }
+
 .volk {
   font-family: 'Vollkorn', serif;
   font-size: 30px;
 }
+
 .router {
   font-size: 85%;
 }
+
 button {
   font-family: cursive;
 }
