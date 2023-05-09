6655b508c62deefa0cdbc388ca58863c62226424
CJ Green <chester.green208@gmail.com>
2023-05-08 22:46:31 -0600

    added image to book

--- kaleenaAuthor.client/src/pages/BookPage.vue
+++ kaleenaAuthor.client/src/pages/BookPage.vue
@@ -5,6 +5,14 @@
           <h1 class="text-center">Lonely Cowboy Hearts Series</h1>
         </div>
       </div>
+      <div class="row mt-5">
+        <div class="col-3 p-0">
+          <img src="src\assets\img\stolen-heart-img.png" width="600">
+        </div>
+        <div class="col-9">
+          <h2 class="text-center">Gavin is a lonely cowboy. Sarah needs a fresh start.</h2>
+        </div>
+      </div>
     </div>
   </template>
   
